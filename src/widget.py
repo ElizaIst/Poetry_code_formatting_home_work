bb38d6b763bf11376427b40a9c381906509a0b57
Eliza <mac@MacBook-Pro--Eliza.local>
2026-02-08 00:21:20 +0600

    Final: fix newline at end of file and pass flake8 checks

--- src/widget.py
+++ src/widget.py
@@ -1,21 +1,28 @@
 from src.masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(info: str) -> str:
     """
-        Kart veya hesap bilgisini alır ve uygun maskelemeyi döner.
-        Örn: 'Visa Platinum 7000792289606361' -> 'Visa Platinum 7000 79** **** 6361'
-        """
-    parts = info.split(' ')
-    number = parts[-1] #Son parca numaradir
-    name = " ".join(parts[:-1]) # Geriye kalan her sey isimdir
+    Программа извлекает и маскирует информацию о карте или счете пользователя.
+    Формат ввода: 'Visa Platinum 7000792289606361'
+    """
+    parts = info.split()
+    number = parts[-1]
+    name = " ".join(parts[:-1])
 
-    if name.lower() == "счет":
+    if name.lower().startswith("счет"):
         return f"{name} {get_mask_account(number)}"
     else:
         return f"{name} {get_mask_card_number(number)}"
 
-def get_date(date_string: str) -> str:
-    """ISO formatındaki tarihi 'GG.AA.YYYY' formatına çevirir."""
-    date_part = date_string[:10]
+
+def get_date(date_str: str) -> str:
+    """
+    Преобразует дату из формата ISO в формат 'ДД.ММ.ГГГГ'.
+    Пример: '2024-03-11T02:26:18.671407' -> '11.03.2024'
+    """
+    date_part = date_str.split("T")[0]
     year, month, day = date_part.split("-")
-    # Gün ve Yıl yer değiştirmeli
     return f"{day}.{month}.{year}"
+
+
